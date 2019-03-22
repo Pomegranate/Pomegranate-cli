e0fd653e2d6abd4cadd8212698ac276300363ad9
Jim Bulkowski <jim.b@paperelectron.com>
2019-03-21 22:12:24 -0400

    initial, placeholder for publish

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project pomegranate-cli
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+console.log('placeholder')
